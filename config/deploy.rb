ae7da07009e4b75773fd0f865dfd28e0b7ce07ef
stap780 <panaet80@mail.ru>
2022-08-14 12:45:25 +0300

    change upload

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -38,7 +35,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 set :application, 'roundbed'
-set :repo_url, 'git@github.com:stap780/roundbed.git'
+set :repo_url, 'git://github.com/stap780/roundbed.git'
 set :deploy_to, '/var/www/roundbed'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public', 'lib/tasks', 'storage')
